edfd0b91b2f9db500ae5efc2e4274d9860c1a469
khorlim <khorlim2001@gmail.com>
2024-12-04 17:43:18 +0800

    new

--- lib/src/mobile_validator.dart
+++ lib/src/mobile_validator.dart
@@ -1,63 +1,72 @@
 import 'package:mobile_helper/src/mobile_country/base_mobile_country.dart';
 
 class MobileValidator {
-  static bool isValid(String mobile) {
+  static MobileCountry? getCountryFromMobile(String mobile) {
     //MY
     if (mobile.startsWith(MalaysiaMobileCountry().code)) {
-      return MalaysiaMobileCountry().checkFormat(mobile);
+      return MalaysiaMobileCountry();
     }
 
     //SG
     else if (mobile.startsWith(SingaporeMobileCountry().code)) {
-      return SingaporeMobileCountry().checkFormat(mobile);
+      return SingaporeMobileCountry();
     }
 
     //Brunei
     else if (mobile.startsWith(BruneiMobileCountry().code)) {
-      return BruneiMobileCountry().checkFormat(mobile);
+      return BruneiMobileCountry();
     }
 
     //HongKong
     else if (mobile.startsWith(HongKongMobileCountry().code)) {
-      return HongKongMobileCountry().checkFormat(mobile);
+      return HongKongMobileCountry();
     }
 
     //Indonesia
     else if (mobile.startsWith(IndonesiaMobileCountry().code)) {
-      return IndonesiaMobileCountry().checkFormat(mobile);
+      return IndonesiaMobileCountry();
     }
 
     //Philippines
     else if (mobile.startsWith(PhilippinesMobileCountry().code)) {
-      return PhilippinesMobileCountry().checkFormat(mobile);
+      return PhilippinesMobileCountry();
     }
 
     //India
     else if (mobile.startsWith(IndiaMobileCountry().code)) {
-      return IndiaMobileCountry().checkFormat(mobile);
+      return IndiaMobileCountry();
     }
 
     //Sri Lanka
     else if (mobile.startsWith(SriLankaMobileCountry().code)) {
-      return SriLankaMobileCountry().checkFormat(mobile);
+      return SriLankaMobileCountry();
     }
 
     //Thailand
     else if (mobile.startsWith(ThailandMobileCountry().code)) {
-      return ThailandMobileCountry().checkFormat(mobile);
+      return ThailandMobileCountry();
     }
 
     //England
     else if (mobile.startsWith(EnglandMobileCountry().code)) {
-      return EnglandMobileCountry().checkFormat(mobile);
+      return EnglandMobileCountry();
     }
 
     //Taiwan
     else if (mobile.startsWith(TaiwanMobileCountry().code)) {
-      return TaiwanMobileCountry().checkFormat(mobile);
+      return TaiwanMobileCountry();
     }
 
     //default
+    return null;
+  }
+
+  static bool isValid(String mobile) {
+    MobileCountry? country = getCountryFromMobile(mobile);
+    if (country != null) {
+      return country.checkFormat(mobile);
+    }
+
     return mobile.length > 6;
   }
 }
