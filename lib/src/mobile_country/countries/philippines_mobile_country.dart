be2e2b2c9e6f59a86891bb0a586b3b29c5b40512
khorlim <khorlim2001@gmail.com>
2024-12-26 15:07:52 +0800

    fix phillipines

--- lib/src/mobile_country/countries/philippines_mobile_country.dart
+++ lib/src/mobile_country/countries/philippines_mobile_country.dart
@@ -3,11 +3,12 @@ part of '../base_mobile_country.dart';
 class PhilippinesMobileCountry extends MobileCountry {
   @override
   bool checkFormat(String mobile) {
-    RegExp philippinesPattern = RegExp(r'^(?:\+?63)?9[0-9]{9}$');
+    // Regular expression for validating Philippine mobile numbers
+    RegExp philippinesPattern = RegExp(r'^(?:\+?63|0)?[0-9]{10}$');
 
     // The pattern breakdown:
-    // ^(?:\+?63)? - Matches the optional country code '63' with or without the '+' symbol.
-    // 9[0-9]{9}$ - Matches a '9' followed by exactly 9 digits.
+    // ^(?:\+?63|0)? - Matches the optional country code '+63' or leading '0'.
+    // [0-9]{10}$ - Matches exactly 10 digits for the mobile number.
 
     return philippinesPattern.hasMatch(mobile);
   }
